cf742a46a8e003cde975bc0604c3bb38ee28c987
Kurt <kaphotics@gmail.com>
2021-09-01 19:32:39 -0700

    Allow starting from command line message

--- FlatCrawler.ConsoleApp/Program.cs
+++ FlatCrawler.ConsoleApp/Program.cs
@@ -9,15 +9,37 @@ namespace FlatCrawler.ConsoleApp
         {
             if (args.Length != 0)
                 Crawl(args[0]);
-            else
-                Console.WriteLine("Open the console application with a valid FlatBuffer binary file to begin.");
+            StartCrawl();
+        }
 
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+        private static void StartCrawl()
+        {
+            while (true)
+            {
+                Console.WriteLine("Type \"open\" (no-quotes) and paste the path of the FlatBuffer you would like to analyze");
+                Console.Write(">>> ");
+                var cmd = Console.ReadLine();
+                if (cmd is not { } x)
+                    continue;
+                x = x.Trim();
+                if (x.StartsWith("quit", StringComparison.InvariantCultureIgnoreCase))
+                    break;
+
+                if (!x.StartsWith("open", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine("Try again.");
+                    continue;
+                }
+
+                var space = x.IndexOf(' ');
+                var path = x[(space + 2)..];
+                Crawl(path);
+            }
         }
 
         private static void Crawl(string path)
         {
+            path = Path.TrimEndingDirectorySeparator(path.Replace("\"", ""));
             if (!File.Exists(path))
             {
                 Console.WriteLine("File does not exist.");
