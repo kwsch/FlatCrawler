9306c2b0070a66a60b6d5ec543f3104cbe4cd2fe
Kurt <kaphotics@gmail.com>
2021-09-09 15:19:35 -0700

    Add searching logic with criteria

---
+++ FlatCrawler.Lib/Util/SearchUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FlatCrawler.Lib
+{
+    public static class SearchUtil
+    {
+        public static string? FindFlatFile(string folder, Func<FlatBufferRoot, byte[], bool> criteria)
+        {
+            return FindFlatFiles(folder, criteria).FirstOrDefault();
+        }
+
+        public static IEnumerable<string> FindFlatFiles(string folder, Func<FlatBufferRoot, byte[], bool> criteria)
+        {
+            var files = Directory.EnumerateFiles(folder);
+            return FindFlatFiles(files, criteria);
+        }
+
+        public static IEnumerable<string> FindFlatFiles(IEnumerable<string> files, Func<FlatBufferRoot, byte[], bool> criteria)
+        {
+            foreach (string file in files)
+            {
+                var data = File.ReadAllBytes(file);
+                var root = FlatBufferRoot.Read(0, data);
+                if (criteria(root, data))
+                    yield return file;
+            }
+        }
+    }
+}
