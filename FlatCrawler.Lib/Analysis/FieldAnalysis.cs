c1cfe62f0e6d1dd56ea2b40424c7518546a59034
Kurt <kaphotics@gmail.com>
2022-10-10 10:06:38 -0700

    Expand field analysis for multi inputs
    Can now be run with multiple flatbuffers (different data), and run on multiple sets of nodes if multiple arrays are present in the node tree.

--- FlatCrawler.Lib/Analysis/FieldAnalysis.cs
+++ FlatCrawler.Lib/Analysis/FieldAnalysis.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 
 namespace FlatCrawler.Lib;
@@ -5,25 +6,33 @@ namespace FlatCrawler.Lib;
 public static class FieldAnalysis
 {
     public static FieldAnalysisResult AnalyzeFields(this IArrayNode array, byte[] data)
-        => AnalyzeFields(data, array.Entries.OfType<FlatBufferNodeField>().ToArray());
+        => AnalyzeFields(new FieldNodeSet(data, array.Entries.OfType<FlatBufferNodeField>().ToArray()));
 
     public static FieldAnalysisResult AnalyzeFields(this FlatBufferNodeField node, byte[] data)
-        => AnalyzeFields(data, node);
+        => AnalyzeFields(new FieldNodeSet(data, new[] {node}));
 
-    public static FieldAnalysisResult AnalyzeFields(byte[] data, params FlatBufferNodeField[] entries)
+    public static FieldAnalysisResult AnalyzeFields(params FieldNodeSet[] input)
     {
         var result = new FieldAnalysisResult();
 
         // Scan all fields, determine size first.
-        foreach (var entry in entries)
-            result.ScanFieldSize(entry);
+        foreach (var set in input)
+        {
+            foreach (var entry in set.Entries)
+                result.ScanFieldSize(entry);
+        }
 
         result.GuessOverallType();
 
         // Scan all fields, determine type.
-        foreach (var entry in entries)
-            result.ScanFieldType(entry, data);
+        foreach (var (data, entries) in input)
+        {
+            foreach (var entry in entries)
+                result.ScanFieldType(entry, data);
+        }
 
         return result;
     }
 }
+
+public sealed record FieldNodeSet(byte[] Data, IReadOnlyList<FlatBufferNodeField> Entries);
